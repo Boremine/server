7d1c47ee0d2c6c8cede17d0c5678cd2ae0a55f06
cawilo <finished92for@gmail.com>
2022-07-06 13:56:43 -0400

    fix cookie 11

--- src/features/Logout/controllers.ts
+++ src/features/Logout/controllers.ts
@@ -1,24 +1,24 @@
 import { Request, Response, NextFunction } from 'express'
-// import { HandleError } from '../../responses/error/HandleError'
-// import { findUserAgent, TokenFound } from '../../utils/Logs/functions/findLog'
+import { HandleError } from '../../responses/error/HandleError'
+import { findUserAgent, TokenFound } from '../../utils/Logs/functions/findLog'
 
-// import User from '../../models/user'
-// import RefreshToken from '../../models/refreshToken'
+import User from '../../models/user'
+import RefreshToken from '../../models/refreshToken'
 import { HandleSuccess } from '../../responses/success/HandleSuccess'
 
 export const logout = async (req: Request, res: Response, next: NextFunction) => {
-  // const { user_id } = res.locals
+  const { user_id } = res.locals
 
-  // const user = await User.findById(user_id).populate({
-  //   path: 'logs',
-  //   populate: { path: 'refreshToken_id' }
-  // })
-  // if (!user) return next(HandleError.Unauthorized('User not found'))
+  const user = await User.findById(user_id).populate({
+    path: 'logs',
+    populate: { path: 'refreshToken_id' }
+  })
+  if (!user) return next(HandleError.Unauthorized('User not found'))
 
-  // const tokenOwner: TokenFound | boolean = findUserAgent(req.useragent, user.logs)
-  // if (!tokenOwner) return next(HandleError.Unauthorized('Invalid Refresh Token'))
+  const tokenOwner: TokenFound | boolean = findUserAgent(req.useragent, user.logs)
+  if (!tokenOwner) return next(HandleError.Unauthorized('Invalid Refresh Token'))
 
-  // await RefreshToken.findById(tokenOwner._id).remove()
+  await RefreshToken.findById(tokenOwner._id).remove()
 
   res.clearCookie('refresh_token', { path: '/', domain: process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test' ? '' : 'boremine.com' })
   res.clearCookie('access_token', { path: '/', domain: process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test' ? '' : 'boremine.com' })
