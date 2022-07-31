a1a84b0776e7f264e763d721570f78d0c20ebb9d
cawilo <finished92for@gmail.com>
2022-07-31 12:48:02 -0400

    setup comments limit

--- src/features/Mural/validators.ts
+++ src/features/Mural/validators.ts
@@ -2,6 +2,19 @@ import { Request, Response, NextFunction } from 'express'
 import User from '../../models/user'
 import { HandleError } from '../../responses/error/HandleError'
 
+export const getOnePieceComments = async (req: Request, res: Response, next: NextFunction) => {
+    const { sort, type } = req.query
+
+    if (sort === 'new') res.locals.sort = 'new'
+    else res.locals.sort = 'top'
+
+    if (type === 'comments') res.locals.type = 'comments'
+    else if (type === 'chatto') res.locals.type = 'chatto'
+    else res.locals.type = 'all'
+
+    next()
+}
+
 export const markPiece = async (req: Request, res: Response, next: NextFunction) => {
     const { mark, user_id } = res.locals
 
