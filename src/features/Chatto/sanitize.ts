5c93d46beb6000d876cc81e326c41d3e6cdbef3d
cawilo <finished92for@gmail.com>
2022-12-07 18:03:20 -0500

    temporal access all

--- src/features/Chatto/sanitize.ts
+++ src/features/Chatto/sanitize.ts
@@ -11,3 +11,19 @@ export const sendMessage = (req: Request, res: Response, next: NextFunction) => 
 
     next()
 }
+
+interface sendMessageBodyNotAuthenticated {
+    message:string
+    nact: number
+    naid: string
+}
+
+export const sendMessageNotAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+    const body:sendMessageBodyNotAuthenticated = req.body
+
+    body.message = String(body.message).trim()
+    body.nact = Number(body.nact)
+    body.naid = String(body.naid)
+
+    next()
+}
