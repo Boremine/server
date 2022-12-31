06b2c7202c4ba2f053ef58bb5a1af3d11d8c0812
cawilo <finished92for@gmail.com>
2022-12-30 22:27:56 -0500

    remove redis

--- src/features/Login/routes.ts
+++ src/features/Login/routes.ts
@@ -17,28 +17,28 @@ import {
     loginTry as loginTry_CONTROLLER
 } from './controllers'
 
-const client = createClient({ url: process.env.REDIS_CONNECTION, password: process.env.REDIS_PASSWORD, username: process.env.REDIS_USERNAME })
-client.connect()
-client.on('connect', () => {
-    console.log('Redis Connected (Login/Try)')
-})
-client.on('error', (error) => {
-    console.error(error)
-})
+// const client = createClient({ url: process.env.REDIS_CONNECTION, password: process.env.REDIS_PASSWORD, username: process.env.REDIS_USERNAME })
+// client.connect()
+// client.on('connect', () => {
+//     console.log('Redis Connected (Login/Try)')
+// })
+// client.on('error', (error) => {
+//     console.error(error)
+// })
 
-const loginTry_LIMITER = rateLimit({
-    windowMs: 10000,
-    max: 5,
-    standardHeaders: true,
-    message: 'To many requests, wait a moment',
-    keyGenerator: (request, response) => `login ${response.locals.user_id} ${request.useragent?.ip}`,
-    store: new RedisStore({
-        sendCommand: (...args: string[]) => client.sendCommand(args)
-    })
-})
+// const loginTry_LIMITER = rateLimit({
+//     windowMs: 10000,
+//     max: 5,
+//     standardHeaders: true,
+//     message: 'To many requests, wait a moment',
+//     keyGenerator: (request, response) => `login ${response.locals.user_id} ${request.useragent?.ip}`,
+//     store: new RedisStore({
+//         sendCommand: (...args: string[]) => client.sendCommand(args)
+//     })
+// })
 
 const router: Router = Router()
 
-router.post('/try', loginTry_LIMITER, loginTry_SANITIZE, loginTry_VALIDATORS, compareLogs, loginTry_CONTROLLER)
+router.post('/try', loginTry_SANITIZE, loginTry_VALIDATORS, compareLogs, loginTry_CONTROLLER)
 
 export default router
