80b2e4b689c749add0a3d2457a6afcec3d494e8e
cawilo <finished92for@gmail.com>
2022-12-30 22:27:56 -0500

    remove redis

--- src/features/Chatto/routes.ts
+++ src/features/Chatto/routes.ts
@@ -23,30 +23,30 @@ import {
     authorize
 } from '../../utils/Authorize/middleware/verifyToken'
 
-const client = createClient({ url: process.env.REDIS_CONNECTION, password: process.env.REDIS_PASSWORD, username: process.env.REDIS_USERNAME })
-client.connect()
-client.on('connect', () => {
-    console.log('Redis Connected (Chatto/Post)')
-})
-client.on('error', (error) => {
-    console.error(error)
-})
+// const client = createClient({ url: process.env.REDIS_CONNECTION, password: process.env.REDIS_PASSWORD, username: process.env.REDIS_USERNAME })
+// client.connect()
+// client.on('connect', () => {
+//     console.log('Redis Connected (Chatto/Post)')
+// })
+// client.on('error', (error) => {
+//     console.error(error)
+// })
 
-const sendMessage_LIMITER = rateLimit({
-    windowMs: 3000,
-    max: 1,
-    standardHeaders: true,
-    message: 'To many requests, wait a moment',
-    keyGenerator: (request, response) => `chatto ${response.locals.user_id} ${request.useragent?.ip}`,
-    store: new RedisStore({
-        sendCommand: (...args: string[]) => client.sendCommand(args)
-    })
-})
+// const sendMessage_LIMITER = rateLimit({
+//     windowMs: 3000,
+//     max: 1,
+//     standardHeaders: true,
+//     message: 'To many requests, wait a moment',
+//     keyGenerator: (request, response) => `chatto ${response.locals.user_id} ${request.useragent?.ip}`,
+//     store: new RedisStore({
+//         sendCommand: (...args: string[]) => client.sendCommand(args)
+//     })
+// })
 
 const router: Router = Router()
 
-router.post('/post', authorize, sendMessage_LIMITER, sendMessage_SANITIZE, sendMessage_VALIDATOR, sendMessage_CONTROLLER)
+router.post('/post', authorize, sendMessage_SANITIZE, sendMessage_VALIDATOR, sendMessage_CONTROLLER)
 
-router.post('/post/notAuthenticated', sendMessage_LIMITER, sendMessageNotAuthenticated_SANITIZE, sendMessageNotAuthenticated_VALIDATOR, sendMessageNotAuthenticated_CONTROLLER)
+router.post('/post/notAuthenticated', sendMessageNotAuthenticated_SANITIZE, sendMessageNotAuthenticated_VALIDATOR, sendMessageNotAuthenticated_CONTROLLER)
 
 export default router
