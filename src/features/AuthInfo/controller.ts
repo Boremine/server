da54cef976b56e8e6a143e570ecb1226f3df89ac
cawilo <finished92for@gmail.com>
2022-12-30 22:06:08 -0500

    env fix 3

--- src/features/AuthInfo/controller.ts
+++ src/features/AuthInfo/controller.ts
@@ -6,18 +6,18 @@ import { HandleError } from '../../responses/error/HandleError'
 import { HandleSuccess } from '../../responses/success/HandleSuccess'
 import { checkIfFirstFive } from '../../utils/Prompts/functions/checkIfFirstFive'
 
-// import { SecretManagerServiceClient } from '@google-cloud/secret-manager'
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager'
 
-// const client = new SecretManagerServiceClient()
+const client = new SecretManagerServiceClient()
 
-// const getSecretValue = async (secretName: string) => {
-//     const [secret] = await client.accessSecretVersion({
-//         name: secretName
-//     })
-//     console.log(secret.payload)
-//     // if (secret.payload) console.log(secret.payload.data.toString())
-//     // return secret.payload.data.toString()
-// }
+const getSecretValue = async (secretName: string) => {
+    const [secret] = await client.accessSecretVersion({
+        name: secretName
+    })
+    console.log(secret.payload)
+    // if (secret.payload) console.log(secret.payload.data.toString())
+    // return secret.payload.data.toString()
+}
 
 export const getAuth = async (req: Request, res: Response, next: NextFunction) => {
     const { user_id } = res.locals
@@ -37,7 +37,7 @@ export const getAuth = async (req: Request, res: Response, next: NextFunction) =
         user.prompt_id.promptInFirstFive = await checkIfFirstFive(user.prompt_id._id)
     }
 
-    // await getSecretValue('projects/boremine/secrets/TEST_ENV')
+    await getSecretValue('projects/boremine/secrets/TEST_ENV')
 
     HandleSuccess.Ok(res, { ...user, auth: true })
 }
